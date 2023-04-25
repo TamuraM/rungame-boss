bfae3fa683261618b7cf59de93452394fce581d3
CHARA\meich <mei.charaa@gmail.com>
2023-04-25 17:15:09 +0900

    オブジェクトを動かす

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+using Cinemachine.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField, Header("移動スピード")] private float _speed = 5f;
+    [SerializeField] private Rigidbody _rigidbody;
+    private bool _isStop = false;
+    [SerializeField, Header("ゴールのオブジェクト")] private Transform _goalObj;
+    private Vector3 _forward;
+
+    private void Start()
+    {
+        _forward = transform.forward;
+        _rigidbody.velocity = _forward * _speed;
+    }
+
+    void Update()
+    {
+
+        if (_isStop) return;
+
+        if (gameObject.transform.position.z > _goalObj.position.z)
+        {
+            _rigidbody.velocity = Vector3.zero;
+            transform.position = new Vector3(_goalObj.position.x, transform.position.y, _goalObj.position.z);
+            _isStop = true;
+        }
+
+    }
+}
