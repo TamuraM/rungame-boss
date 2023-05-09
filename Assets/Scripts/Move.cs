402b8afc5183fd29238a0933d53ec2ccb01ea724
CHARA\meich <mei.charaa@gmail.com>
2023-05-09 16:15:52 +0900

    インプット

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
     [SerializeField, Header("移動スピード")] private float _speed = 5f;
     [SerializeField] private Rigidbody _rigidbody;
     private bool _isStop = false;
-    [SerializeField, Header("ゴールのオブジェクト")] private Transform _goalObj;
+    [SerializeField, Header("ゴールの位置")] private Transform _goalObj;
     private Vector3 _forward;
+    private InputAction _moveAction = default;
+    [SerializeField] private const float MOVE_RANGE = 8;
 
     private void Start()
     {
+        //--InputSystem使ってみた--//
+        var playerInput = GetComponent<PlayerInput>();
+        var actionMap = playerInput.currentActionMap;
+        _moveAction = actionMap["Move"];
+        //--InputSystem使ってみた--// 参考:https://gamedev65535.com/entry/unity_inputsystem_howtouse/#i-2
         _forward = transform.forward;
         _rigidbody.velocity = _forward * _speed;
     }
@@ -24,8 +32,22 @@ public class Move : MonoBehaviour
         if (gameObject.transform.position.z > _goalObj.position.z)
         {
             _rigidbody.velocity = Vector3.zero;
-            transform.position = new Vector3(_goalObj.position.x, transform.position.y, _goalObj.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y, _goalObj.position.z);
             _isStop = true;
+            return;
+        }
+
+        var move = _moveAction.ReadValue<float>();
+        _rigidbody.velocity = new Vector3(move * _speed, _rigidbody.velocity.y, _rigidbody.velocity.z);
+
+        //プレイヤーが外側に出れないように
+        if(transform.position.x < -MOVE_RANGE) //左端
+        {
+            transform.position = new Vector3(-MOVE_RANGE, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x > MOVE_RANGE) //右端
+        {
+            transform.position = new Vector3(MOVE_RANGE, transform.position.y, transform.position.z);
         }
 
     }
